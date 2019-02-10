bbac58524ff983f19e8bf15f0449c2544a429ad6
Rafael1991 <n-fabrizius@web.de>
2019-02-10 16:42:11 +0100

    MemoryFinal
    cardboard and fullscreen work on Android 7 - 8.1

---
+++ Assets/FoundPair.cs
@@ -0,0 +1,192 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Text.RegularExpressions;
+using Vuforia;
+using System.Linq;
+
+public class FoundPair : MonoBehaviour
+{
+    public AudioSource audioS;
+    public AudioClip soundS;
+    private TrackableBehaviour mTrackableBehaviour;
+    private int Count;
+    public string target1;
+    public string target2;
+    private AudioSource currentAudio;
+    public AudioSource rightAudio;
+    public string tempPart1;
+    public string tempPart2;
+    public int NumberOfCurrentTargets;
+    public bool Winning;
+    public bool MatchFound = false;
+    public float Timer1;
+    public float FinishTimer = 0f;
+    public bool needTimer = false;
+    public ParticleSystem WinningParticle;
+    public TextMesh WinningText;
+    public AudioSource WinningSound;
+    public AudioClip WinningClip;
+    public int NumberOfCurrentSounds;
+    public float SoundTimer = 0f;
+    public bool bPlayingWinningSound = false;
+    public float MaxPlaySoundTimer = 3f;
+
+
+    void Start()
+    {
+        //currentAudio = GameObject.Find("currentAudio").GetComponent<AudioSource>();
+        //rightAudio = GameObject.Find("rightAudio").GetComponent<AudioSource>();
+
+    }
+
+    public void FoundImage()
+    {
+        needTimer = false;
+        Timer1 = 0f;
+    }
+
+    public void LostImage()
+    {
+        Debug.Log("Lost Image");
+        MatchFound = false;
+        needTimer = true;
+        Timer1 = 0f;
+    }
+
+    public void Reset()
+    {
+        target1 = null;
+        target2 = null;
+        tempPart1 = null;
+        tempPart2 = null;
+        MatchFound = false;
+    }
+
+    public void Timer()
+    {
+        if(Timer1 > 5f)
+        {
+            Reset();
+            needTimer = false;
+            Timer1 = 0f;
+        }
+    }
+
+    public void F_finishTimer()
+    {
+        if (FinishTimer > 3f)
+        {
+            WinningSound.Stop();
+            //WinningParticle.Stop();
+            FinishTimer = 0f;
+            WinningText.GetComponentInChildren<Renderer>().enabled = false;
+            //WinningParticle.GetComponentInChildren<Renderer>().enabled = false;
+            Winning = false;
+        }
+    }
+
+    public void F_Winning()
+    {
+        //gibiihnwin
+        //WinningSound.PlayOneShot(WinningClip);
+        MatchFound = true;
+        Debug.Log("Yeahaaa we got a match!");
+        //Debug.Log(WinningSound);
+        //WinningParticle.Play();
+        WinningText.GetComponentInChildren<Renderer>().enabled = true;
+        //WinningParticle.GetComponentInChildren<Renderer>().enabled = true;
+        Winning = true;
+
+    }
+
+    public void S_Winning()
+    {
+           
+        if(!bPlayingWinningSound)
+        {
+            WinningSound.PlayOneShot(WinningClip);
+            Debug.Log(WinningSound);
+            bPlayingWinningSound = true;
+        }
+        
+
+        /*if (!bPlayingWinningSound == true && SoundTimer > 5)
+        {
+            WinningSound.PlayOneShot(WinningClip);
+            Debug.Log(WinningSound);
+            bPlayingWinningSound = true;
+            SoundTimer += Time.deltaTime;
+            if(SoundTimer == 5)
+            {
+                bPlayingWinningSound = false;
+            }
+        }*/
+        
+    }
+
+    void Update()
+    {
+        if (bPlayingWinningSound)
+        {
+            SoundTimer += Time.deltaTime;
+            if (SoundTimer >= MaxPlaySoundTimer)
+            {
+                bPlayingWinningSound = false;
+                SoundTimer = 0f;
+            }
+        }
+        if (Winning)
+        {
+            FinishTimer += Time.deltaTime;
+        }
+
+        if (needTimer)
+        {
+            Timer1 += Time.deltaTime;
+        }
+        Timer();
+        F_finishTimer();
+        // Get the Vuforia StateManager
+        StateManager sm = TrackerManager.Instance.GetStateManager();
+
+        // Query the StateManager to retrieve the list of
+        // currently 'active' trackables 
+        //(i.e. the ones currently being tracked by Vuforia)
+        IEnumerable<TrackableBehaviour> activeTrackables = sm.GetActiveTrackableBehaviours();
+
+        NumberOfCurrentTargets = activeTrackables.Count();
+        NumberOfCurrentSounds = activeTrackables.Count();
+
+        // Iterate through the list of active trackables
+        Debug.Log("List of trackables currently active (tracked): ");
+        foreach (TrackableBehaviour tb in activeTrackables)
+        {
+            if (Regex.IsMatch(tb.TrackableName, "1", RegexOptions.IgnoreCase))
+            {
+                target1 = tb.TrackableName;
+            }
+            else
+            {
+                target2 = tb.TrackableName;
+            }
+
+            if (target1 != null && target2 != null)
+            {
+                tempPart1 = Regex.Replace(target1, "1", "");
+                tempPart2 = Regex.Replace(target2, "2", "");
+
+                if (tempPart1 == tempPart2)
+                {
+                    F_Winning();
+                    S_Winning();
+
+                }
+            }
+        }
+        if (NumberOfCurrentTargets == 0)
+        {
+
+        }
+    }
+}
---
+++ Assets/PlaySound.cs
@@ -0,0 +1,235 @@
+﻿
+using UnityEngine;
+using Vuforia;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Vuforia
+{
+
+    /// <summary>
+    ///     A custom handler that implements the ITrackableEventHandler interface.
+    /// </summary>
+    public class PlaySound : MonoBehaviour, ITrackableEventHandler
+    {
+
+        public FoundPair FPair;
+
+        //------------Begin Sound----------
+        public AudioSource soundTarget;
+        public AudioClip clipTarget;
+        private AudioSource[] allAudioSources;
+
+        //function to stop all sounds_Nikita
+        void StopAllAudio()
+        {
+            allAudioSources = FindObjectsOfType(typeof(AudioSource)) as AudioSource[];
+            foreach (AudioSource audioS in allAudioSources)
+            {
+                audioS.Stop();
+            }
+        }
+
+        //function to play sound _ Níkita
+        void playSound(string soundString)
+        {
+            clipTarget = (AudioClip)Resources.Load(soundString);
+            soundTarget.clip = clipTarget;
+            soundTarget.loop = false;
+            soundTarget.playOnAwake = false;
+            soundTarget.Play();
+
+        }
+
+        //-----------End Sound------------
+
+        #region PRIVATE_MEMBER_VARIABLES
+
+        private TrackableBehaviour mTrackableBehaviour;
+
+        #endregion // PRIVATE_MEMBER_VARIABLES
+
+        #region UNTIY_MONOBEHAVIOUR_METHODS
+
+        void Start()
+        {
+            FPair = FindObjectOfType<FoundPair>();
+            mTrackableBehaviour = GetComponent<TrackableBehaviour>();
+            if (mTrackableBehaviour)
+            {
+                mTrackableBehaviour.RegisterTrackableEventHandler(this);
+            }
+
+            //Register / add the AudioSource as object
+            soundTarget = (AudioSource)gameObject.AddComponent<AudioSource>();
+
+        }
+
+        #endregion // UNTIY_MONOBEHAVIOUR_METHODS
+
+        #region PUBLIC_METHODS
+
+        /// <summary>
+        ///     Implementation of the ITrackableEventHandler function called when the
+        ///     tracking state changes.
+        /// </summary>
+        public void OnTrackableStateChanged(
+            TrackableBehaviour.Status previousStatus,
+            TrackableBehaviour.Status newStatus)
+        {
+            if (newStatus == TrackableBehaviour.Status.DETECTED ||
+                newStatus == TrackableBehaviour.Status.TRACKED ||
+                newStatus == TrackableBehaviour.Status.EXTENDED_TRACKED)
+            {
+                Debug.Log("Trackable " + mTrackableBehaviour.TrackableName + " found");
+                OnTrackingFound();
+            }
+            else if (previousStatus == TrackableBehaviour.Status.TRACKED &&
+                     newStatus == TrackableBehaviour.Status.NO_POSE)
+            {
+                Debug.Log("Trackable " + mTrackableBehaviour.TrackableName + " lost");
+                OnTrackingLost();
+            }
+            else
+            {
+                // For combo of previousStatus=UNKNOWN + newStatus=UNKNOWN|NOT_FOUND
+                // Vuforia is starting, but tracking has not been lost or found yet
+                // Call OnTrackingLost() to hide the augmentations
+                OnTrackingLost();
+            }
+        }
+
+        #endregion // PUBLIC_METHODS
+
+        #region PRIVATE_METHODS
+
+        private void OnTrackingFound()
+        {
+            var rendererComponents = GetComponentsInChildren<Renderer>(true);
+            var colliderComponents = GetComponentsInChildren<Collider>(true);
+            var canvasComponents = GetComponentsInChildren<Canvas>(true);
+
+            // Enable rendering:
+            foreach (var component in rendererComponents)
+            {
+                component.enabled = true;
+            }
+            // Enable colliders:
+            foreach (var component in colliderComponents)
+            {
+                component.enabled = true;
+            }
+            // Enable canvas':
+            foreach (var component in canvasComponents)
+            {
+                component.enabled = true;
+            }
+
+            //Play Sound, IF detect an Imagetarget
+            foundImage_playSound();
+
+        }
+
+        // Method to play Sound, IF an Imagetarget the picture is detected _ Nikita
+        public void foundImage_playSound()
+        {
+
+            if (mTrackableBehaviour.TrackableName == "bird1" || mTrackableBehaviour.TrackableName == "bird2" ||
+                mTrackableBehaviour.TrackableName == "bird1" && mTrackableBehaviour.TrackableName == "bird2")
+            {
+                playSound("sounds/blueJay");
+
+            }
+
+            if (mTrackableBehaviour.TrackableName == "Butterfly1" || mTrackableBehaviour.TrackableName == "Butterfly2" ||
+                mTrackableBehaviour.TrackableName == "Butterfly1" && mTrackableBehaviour.TrackableName == "Butterfly2")
+            {
+                playSound("sounds/Butterfly");
+                soundTarget.loop = true;
+
+            }
+
+            if (mTrackableBehaviour.TrackableName == "gwcat1" || mTrackableBehaviour.TrackableName == "gwcat2"
+                || mTrackableBehaviour.TrackableName == "gwcat1" && mTrackableBehaviour.TrackableName == "gwcat2")
+            {
+                playSound("sounds/Die Meows 1");
+            }
+
+            if (mTrackableBehaviour.TrackableName == "corgibrown1" || mTrackableBehaviour.TrackableName == "corgibrown2" ||
+                mTrackableBehaviour.TrackableName == "corgibrown1" && mTrackableBehaviour.TrackableName == "corgibrown2")
+            {
+                playSound("sounds/Dogs Bark #4");
+            }
+
+            if (mTrackableBehaviour.TrackableName == "blackcat1" || mTrackableBehaviour.TrackableName == "blackcat2" ||
+                mTrackableBehaviour.TrackableName == "blackcat1" && mTrackableBehaviour.TrackableName == "blackcat2")
+            {
+                playSound("sounds/Meows #6");
+            }
+
+            if (mTrackableBehaviour.TrackableName == "gcat1" || mTrackableBehaviour.TrackableName == "gcat2"
+                || mTrackableBehaviour.TrackableName == "gcat1" && mTrackableBehaviour.TrackableName == "gcat2")
+            {
+                playSound("sounds/Meows #4");
+            }
+
+            if (mTrackableBehaviour.TrackableName == "husky1" || mTrackableBehaviour.TrackableName == "husky2" ||
+                mTrackableBehaviour.TrackableName == "husky1" && mTrackableBehaviour.TrackableName == "husky2")
+            {
+                playSound("sounds/Dogs Bark #6");
+            }
+
+            if (mTrackableBehaviour.TrackableName == "corgiwhite1" || mTrackableBehaviour.TrackableName == "corgiwhite2" ||
+                mTrackableBehaviour.TrackableName == "corgiwhite1" && mTrackableBehaviour.TrackableName == "corgiwhite2")
+            {
+                playSound("sounds/Dogs Bark #6");
+            }
+
+            if (mTrackableBehaviour.TrackableName == "whale1" || mTrackableBehaviour.TrackableName == "whale2" ||
+                mTrackableBehaviour.TrackableName == "wha1e1" && mTrackableBehaviour.TrackableName == "whale2")
+            {
+                playSound("sounds/Whale #2");
+            }
+
+            if (mTrackableBehaviour.TrackableName == "lizard1" || mTrackableBehaviour.TrackableName == "lizard2" ||
+                mTrackableBehaviour.TrackableName == "lizard1" && mTrackableBehaviour.TrackableName == "lizard2")
+            {
+                playSound("sounds/lizard #2");
+            }
+
+            FPair.FoundImage();
+        }
+
+
+        private void OnTrackingLost()
+        {
+            var rendererComponents = GetComponentsInChildren<Renderer>(true);
+            var colliderComponents = GetComponentsInChildren<Collider>(true);
+            var canvasComponents = GetComponentsInChildren<Canvas>(true);
+
+            // Disable rendering:
+            foreach (var component in rendererComponents)
+            {
+                component.enabled = false;
+            }
+            // Disable colliders:
+            foreach (var component in colliderComponents)
+            {
+                component.enabled = false;
+            }
+            // Disable canvas':
+            foreach (var component in canvasComponents)
+            {
+                component.enabled = false;
+            }
+            Debug.Log("Trackable " + mTrackableBehaviour.TrackableName + " lost");
+
+            //Stop All Sounds if Target Lost
+            StopAllAudio();
+
+            FPair.LostImage();
+        }
+
+        #endregion // PRIVATE_METHODS
+    }
+}
